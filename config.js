8fc97fb7471b7a43e94791208f42b40cdab11d2d
Ender Studios <76491740+sairamkishan300@users.noreply.github.com>
2026-02-10 04:13:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ const CONFIG = {
     googleClientId: "1011495957114-jftfnap62g44g6a6l50psgc615tatckr.apps.googleusercontent.com",
 
     // API Endpoint
-    apiUrl: "https://script.google.com/macros/s/AKfycbxTfaWWro6c_Q0rZ5OBaHSBfE7aX2m_qA75ACzbtoDjkERK-uvcxRibLV3Xx9qB8DRDUQ/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycbzbdueLCa2zKPGHmw9RyfTB6IghmaSF3ChtyYtdvjKpuNAPZ43ok6ngOA-inKeZhGmFDw/exec",
 
     // UI Theme Colors
     theme: {
